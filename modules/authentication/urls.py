a5a18a7ce9275b7d2c7f3148529df39c7f76e14b
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-10-11 18:26:27 +0700

    feat: initialize auth module

--- modules/authentication/urls.py
+++ modules/authentication/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from modules.authentication.views import *
 
-app_name = 'main'
+app_name = 'auth'
 
 urlpatterns = [
-    path('', login, name='main'),
+    path('/login', login_user, name='login'),
+    path('/logout', logout_user, name='logout'),
+    path('/register', register, name='register'),
+    path('/profile', profile, name='profile')
 ]
--- modules/authentication/views.py
+++ modules/authentication/views.py
@@ -1,20 +1,67 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import login, logout
+from django.contrib import messages
+import datetime
 
 # Create your views here.
-def login(request):
-    pass
+def login_user(request):
+    if (request.user.is_authenticated):return redirect('main:main')
+    
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
 
-def logout(request):
-    pass
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse('main:main'))
+            response.set_cookie('last_login', datetime.datetime.now())
+            return response
+        else:
+            messages.error(request, "Invalid username or password")
+        
+
+    else:
+        form = AuthenticationForm(request)
+
+    context = {'form': form}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('auth:login'))
+    response.delete_cookie('last_login')
+    return response
 
 def register(request):
-    pass
+    form = UserCreationForm()
+    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully.')
+            
+            return redirect('auth:login')
+        
+    context = {
+        'form': form
+    }
+    
+    return render(request, 'register.html', context)
 
 def profile(request):
-    pass
+    user = request.user
+    
+    context = {
+        'user': user
+    }
+    
+    return render(request, 'profile.html', context)
 
 def edit_profile(request):
-    pass
-
-def change_password(request):
     pass
