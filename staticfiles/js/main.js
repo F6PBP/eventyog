8f9a9d6609add88724c9106c73656f8a5603ed59
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-10-12 09:37:22 +0700

    feat: add modal ds

--- staticfiles/js/main.js
+++ staticfiles/js/main.js
@@ -1,1 +1,15 @@
-console.log("HELLO WORLDSSS");
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = 'flex';  // Show modal (set to flex)
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = 'none';  // Hide modal
+}
+
+// Close modal when clicking on 'data-dismiss' buttons
+document.querySelectorAll('[data-dismiss="modal"]').forEach(button => {
+    button.addEventListener('click', function () {
+        const modal = this.closest('[role="dialog"]');
+        closeModal(modal.id);
+    });
+});
