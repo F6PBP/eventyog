892c925a402137857977264ce040112d27a27590
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-10-22 23:49:15 +0700

    feat: add github repo link to about page

--- modules/main/models.py
+++ modules/main/models.py
@@ -17,7 +17,6 @@ class UserProfile(models.Model):
     
     registeredEvent = models.ManyToManyField('Event', blank=True)
     boughtMerch = models.ManyToManyField('Merchandise', blank=True)
-    
     friends = models.ManyToManyField('UserProfile', blank=True)
     
     role = models.CharField(
@@ -28,6 +27,16 @@ class UserProfile(models.Model):
     
     def __str__(self):
         return self.user.username
+
+class MerchCart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    merchandise = models.ForeignKey('Merchandise', on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def totalPrice(self):
+        return self.merchandise.price * self.quantity
 
 # Class for Event Category
 class EventCategory(models.TextChoices):
@@ -40,39 +49,21 @@ class EventCategory(models.TextChoices):
     LAINNYA='LN', 'Lainnya'
 
 class TicketPrice(models.Model):
+    event = models.ForeignKey('Event', on_delete=models.CASCADE, related_name='ticketPrice')
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    event = models.ForeignKey('Event', on_delete=models.CASCADE, related_name='ticketPrice')
     
     def isFree(self):
         return self.price == 0
 
 class Rating(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     rated_event = models.ForeignKey('Event', on_delete=models.CASCADE)
     rating = models.IntegerField()
     review = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
-class PostImage(models.Model):
-    image = CloudinaryField('image')
-    
-class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    images = models.ManyToManyField(PostImage, blank=True)
-    related_event = models.ForeignKey('Event', on_delete=models.CASCADE, null=True)
-    last_update = models.DateTimeField(auto_now=True)
-    like = models.IntegerField(default=0)
-    description = models.TextField(null=True, blank=True)
-    comments = models.ManyToManyField('PostComment', blank=True)
-    
-class PostComment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    last_update = models.DateTimeField(auto_now=True)
-    like = models.IntegerField(default=0)
-    reply_to = models.ForeignKey('PostComment', on_delete=models.CASCADE, null=True)
-
 class Event(models.Model):
     uuid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     title = models.CharField(max_length=200)
@@ -96,11 +87,10 @@ class Event(models.Model):
     
 
 class Merchandise(models.Model):
-    merchId = models.AutoField(primary_key=True)
+    merchUuid = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     image_url = models.URLField()
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.IntegerField(default=10)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
