ec3a8fa3face020c5ab8781670fbb94e178eebae
FransiscaEllyaB <ellyafransiscab@gmail.com>
2024-10-26 04:32:07 +0700

    delete, edit, and add

--- modules/yogevent/urls.py
+++ modules/yogevent/urls.py
@@ -4,14 +4,13 @@ from modules.yogevent.views import *
 app_name = 'yogevent'
 
 urlpatterns = [
-    path('', show_main, name='main'),
+    path('', main, name='main'),
     path('xml/', show_event_xml, name='show_xml'),
     path('json/', show_event_json, name='show_json'),
     path('xml/<str:id>/', show_xml_event_by_id, name='show_xml_by_id'),
     path('json/<str:id>/', show_json_event_by_id, name='show_json_by_id'),
-    path('admin/create_event/', create_event_entry, name='create_event'),
     path('detail-event/<uuid:uuid>/', detail_event, name='detail_event'),
     path('edit-event/<uuid:uuid>/', edit_event, name='edit_event'),
     path('delete-event/<uuid:uuid>/', delete_event, name='delete_event'),
-    path('create-event-entry-ajax', create_event_entry_ajax, name="create_event_entry_ajax"), 
+    path('create-event', create_event_entry_ajax, name="create_event_entry_ajax"), 
 ]
--- modules/yogevent/views.py
+++ modules/yogevent/views.py
@@ -10,14 +10,13 @@ from django.utils.html import strip_tags
 from django.db.models import Q
 
 @check_user_profile(is_redirect=True)
-def show_main(request: HttpRequest) -> HttpResponse:
-    user_profiles = UserProfile.objects.all()
+def main(request: HttpRequest) -> HttpResponse:
     events = Event.objects.all()
     query = request.GET.get('q')
     category = request.GET.get('category')
 
     if query:
-        events = events.filter(Q(title__icontains=query) | Q(description__icontains=query))
+        events = events.filter(Q(title__icontains=query))
 
     if category:
         events = events.filter(category=category)
@@ -29,7 +28,6 @@ def show_main(request: HttpRequest) -> HttpResponse:
         'show_navbar': True,
         'show_footer': True,
         'is_admin': request.user_profile.role == 'AD',
-        'users': user_profiles,
         'events': events,
     }
     return render(request, 'yogevent.html', context)
@@ -49,6 +47,7 @@ def show_event_json(request):
         events = Event.objects.filter(userprofile=user_profile)
     else:
         events = Event.objects.all()
+
     return HttpResponse(serializers.serialize("json", events), content_type="application/json") 
 
 def show_xml_event_by_id(request, id):
@@ -63,56 +62,44 @@ def show_json_event_by_id(request, id):
 @csrf_exempt
 @require_POST
 def create_event_entry_ajax(request):
-    categories = [
-        ('OL', 'Olahraga'),
-        ('SN', 'Seni'),
-        ('MS', 'Musik'),
-        ('CP', 'Cosplay'),
-        ('LG', 'Lingkungan'),
-        ('VL', 'Volunteer'),
-        ('AK', 'Akademis'),
-        ('KL', 'Kuliner'),
-        ('PW', 'Pariwisata'),
-        ('FS', 'Festival'),
-        ('FM', 'Film'),
-        ('FN', 'Fashion'),
-        ('LN', 'Lainnya')
-    ]
+    title = strip_tags(request.POST.get('title'))
+    description = strip_tags(request.POST.get('description'))
+    category = request.POST.get('category')
+    start_time = request.POST.get('start_time')
+    end_time = request.POST.get('end_time', '')
+    location = strip_tags(request.POST.get('location'))
+    image_url = strip_tags(request.POST.get('image_url'))
 
-    user_profile = UserProfile.objects.all()
-    if user_profile.role == "AD":
-        form = EventForm(request.POST or None)
-        if form.is_valid():
-            event_entry = form.save(commit=False)
-            event_entry.user = request.user
-            event_entry.save()
-            return redirect('yogevent:show_main')
+    end_time = end_time if end_time != "" else None
+    if end_time and start_time >= end_time:
+        context = {
+            'message': 'Tidak Bisa Membuat Event karena Acara Berakhir sebelum Dimulai',
+        }
+        return render(request, "error.html", context)
 
-        context = {'form': form, "categories":categories}
-        return render(request, "create_event_entry.html", context)
+    try:
+        new_event = Event(
+            title=title,
+            description=description,
+            category=category,
+            start_time=start_time,
+            end_time=end_time,
+            location=location,
+            image_urls=[image_url]
+        )
+        new_event.save()
+        return redirect('yogevent:main')
+    
+    except Exception as e:
+        context = {
+            'message': 'Error',
+        }
+        return render(request, "error.html", context)
 
-    return HttpResponse(b"CREATED", status=201)
-
-@require_POST
-def create_event_entry(request):
-    user_profile = UserProfile.objects.all()
-    if user_profile.role == "AD":
-        form = EventForm(request.POST or None)
-        if form.is_valid():
-            event_entry = form.save(commit=False)
-            event_entry.user = request.user
-            event_entry.save()
-            return redirect('yogevent:show_main')
-
-        context = {'form': form}
-        return render(request, "create_event_entry.html", context)
-
-    return HttpResponse(b"CREATED", status=201)
 
 def detail_event(request, uuid):
     event = get_object_or_404(Event, uuid=uuid)
     user_profile = getattr(request.user, 'userprofile', None)
-    user_profiles = UserProfile.objects.all()
 
     context = {
         'user': request.user,
@@ -121,16 +108,14 @@ def detail_event(request, uuid):
         'show_navbar': True,
         'show_footer': True,
         'is_admin': user_profile.role == 'AD' if user_profile else False,
-        'users': user_profiles,
         'event': event,
     }
     return render(request, 'detail_event.html', context)
 
 def delete_event(request, uuid):
-    print(f"Edit event called with UUID: {uuid}")
     event = get_object_or_404(Event, uuid=uuid)
     event.delete()
-    return HttpResponseRedirect(reverse('yogevent:show_main'))
+    return HttpResponseRedirect(reverse('yogevent:main'))
 
 def edit_event(request, uuid):
 
@@ -139,9 +124,14 @@ def edit_event(request, uuid):
 
     if request.method == "POST" and form.is_valid():  # Check method first
         form.save()
-        return HttpResponseRedirect(reverse('yogevent:show_main'))
-    else:
-        print(form.errors)
+        return HttpResponseRedirect(reverse('yogevent:main'))
 
-    context = {'form': form, 'event': event}
+    context = {
+        'user': request.user,
+        'show_navbar': True,
+        'show_footer': True,
+        'event': event,
+        'form': form,
+    }
+
     return render(request, "edit_event.html", context)
