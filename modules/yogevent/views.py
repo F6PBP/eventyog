12679b07c13919efe5d970902e90e0dc5f5ead55
luveina <saragihluvenia@gmail.com>
2024-10-26 18:39:37 +0700

    ini testing

--- modules/yogevent/views.py
+++ modules/yogevent/views.py
@@ -106,6 +106,7 @@ def create_event_entry_ajax(request):
 def detail_event(request, uuid):
     event = get_object_or_404(Event, uuid=uuid)
     user_profile = getattr(request.user, 'userprofile', None)
+    merchandise = Merchandise.objects.all()
 
     context = {
         'user': request.user,
@@ -115,6 +116,7 @@ def detail_event(request, uuid):
         'show_footer': True,
         'is_admin': user_profile.role == 'AD' if user_profile else False,
         'event': event,
+        'merchandise': merchandise,
     }
     return render(request, 'detail_event.html', context)
 
