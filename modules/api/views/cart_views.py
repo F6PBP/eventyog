4c13b92177a531187101f145721a2cf3a2a32233
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-11-18 16:38:03 +0700

    feat: save

--- modules/api/views/cart_views.py
+++
@@ -1,5 +0,0 @@
-from  django.http import HttpRequest, JsonResponse
-from modules.api.ApiResponse import ApiResponse
-
-def main(request: HttpRequest) -> ApiResponse:
-    return ApiResponse(status=200, content="Hello, World!")
