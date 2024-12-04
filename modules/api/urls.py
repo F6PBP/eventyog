d62d6dfbe26db8861858d17ca1ea95074343070d
Andrew4Coding <andrewdevitoaryo@gmail.com>
2024-12-04 16:08:19 +0700

    feat: profile

--- modules/api/urls.py
+++ modules/api/urls.py
@@ -5,4 +5,6 @@ app_name = 'api'
 urlpatterns = [
     path('auth/login/', auth_views.login, name='auth_login'),
     path('auth/register/', auth_views.register, name='auth_register'),
+    path('auth/logout/', auth_views.logout, name='auth_logout'),
+    path('auth/profile/', auth_views.profile, name='auth_profile'),
 ]
